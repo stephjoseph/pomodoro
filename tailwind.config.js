a614f8ad14792e7896d0fd2441179dc0a2c36d6a
Stephen Muñez <sjtmunez@gmail.com>
2024-02-01 13:30:31 +0800

    setup: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        salmon: '#F87070',
+        turquoise: '#70F3F8',
+        lavender: '#D881F8',
+        'light-grey': '#D7E0FF',
+        'midnight-blue': '#1E213F',
+        white: '#FFFFFF',
+        'pale-grey': '#EFF1FA',
+        'midnight-navy': '#161932',
+      },
+      fontFamily: {
+        sans: ['"Kumbh Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
